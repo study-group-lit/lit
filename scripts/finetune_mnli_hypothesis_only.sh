9ddd62727684f19a028bdd5138a2bb71747e30f0
data-niklas <niklas@4loeser.net>
2023-03-02 00:13:39 +0100

    adds hypothesis only mnli fine-tuning

---
+++ scripts/finetune_mnli_hypothesis_only.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=finetune_mnli_hypothesis_only
+#SBATCH --output=finetune_mnli_hypothesis_only
+#SBATCH --ntasks=1
+#SBATCH --time=72:00:00
+#SBATCH --mem=32000
+#SBATCH --mail-type=ALL
+#SBATCH --partition=students
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=batch
+#SBATCH --gres=gpu
+
+
+PATH=/opt/slurm/bin:$PATH
+
+
+source ~/lit/venv/bin/activate
+python3 finetune_mnli_hypothesis_only.py
