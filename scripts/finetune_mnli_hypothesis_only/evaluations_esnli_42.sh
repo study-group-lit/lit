24ed277ab32479177f24f09ff0943365f2e5d5d4
loeser <loeser@login.cl.uni-heidelberg.de>
2023-03-15 20:10:01 +0100

    shortens file names

---
+++ scripts/finetune_mnli_hypothesis_only/evaluations_esnli_42.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=calculate_evaluations_on_esnli
+#SBATCH --output=calculate_evaluations_on_esnli_42.txt
+#SBATCH --ntasks=1
+#SBATCH --time=72:00:00
+#SBATCH --mem=32000
+#SBATCH --mail-type=ALL
+#SBATCH --partition=students
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=batch
+#SBATCH --gres=gpu
+
+
+PATH=/opt/slurm/bin:$PATH
+
+
+source ../../venv/bin/activate
+python3 calculate_evaluations_on_esnli.py 42
