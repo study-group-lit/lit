2299886ac7e317e4541cfed4cf9437987fccd18c
Andre Trump <anditru@web.de>
2023-03-21 19:21:58 +0100

    Implement requested changes

--- scripts/split_esnli/occurences_by_phenomenon.py
+++ scripts/split_esnli/occurences_by_phenomenon.py
@@ -1,10 +1,18 @@
 from datasets import DatasetDict
 from multiprocessing import cpu_count
+import json
 
-esnli_phenomena = DatasetDict.load_from_disk("../../lit-data/datasets/esnli_phenomena_all_words")
+esnli_phenomena = DatasetDict.load_from_disk("../../../lit-data/datasets/esnli_phenomena_all_words")
 phenomena = ["synonym", "antonym", "hypernym", "hyponym", "co_hyponym", "quantifiers", "numericals"]
 splits = ["train", "test", "validation"]
+occurences = []
+
 for split in splits:
     for phenomenon in phenomena:
         filtered = esnli_phenomena[split].filter(lambda r: r[phenomenon] > 0, num_proc=cpu_count())
-        print(f"{split} {phenomenon}: {filtered.num_rows}")
+        occurences.append({
+          "split": split,
+          "phenomenon": phenomenon,
+          "count": filtered.num_rows
+        })
+print(json.dumps(occurences))
