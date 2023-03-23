87b137e3cc8a5044d597529b538b1b44840ea883
Andre Trump <anditru@web.de>
2023-03-23 12:51:32 +0100

    Calculate metrics by phenomena over all words

---
+++ scripts/metrics_by_phenomena_all_words/evaluate_model.py
@@ -0,0 +1,24 @@
+import os
+from transformers import pipeline
+from evaluate import EvaluationSuite
+from argparse import ArgumentParser
+
+parser = ArgumentParser(
+    prog="Evaluate by phenomena"
+)
+parser.add_argument('-m', '--model_path', required=True)
+args = parser.parse_args()
+
+# check paths and create if needed
+model_path = args.model_path
+if not os.path.exists(model_path):
+    raise IOError("Model path does not exist")
+
+model = pipeline("text-classification", model=model_path)
+
+suite = EvaluationSuite.load("../../datasets/evaluation_suite_phenomena_all_words.py")
+results = suite.run(model)
+
+for result in results:
+    result["task_name"] = result["task_name"].info.description
+print(results)
---
+++ scripts/metrics_by_phenomena_all_words/evaluate_roberta-base-finetuned-mnli-hypothesis-only.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=evaluate_roberta-base-finetuned-mnli-hypothesis-only
+#SBATCH --output=evaluate_roberta-base-finetuned-mnli-hypothesis-only.txt
+#SBATCH --ntasks=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=32000
+#SBATCH --partition=students
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=batch
+#SBATCH --gres=gpu
+
+
+PATH=/opt/slurm/bin:$PATH
+
+
+source ~/lit/venv/bin/activate
+python3 evaluate_model.py -m /workspace/students/lit/models/roberta-base-finetuned-mnli-hypothesis-only/42
