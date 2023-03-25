3dc752fc2ee1922c118fe7f8320de57d1aff31d2
anditru <57002492+anditru@users.noreply.github.com>
2023-03-25 10:29:12 +0100

    add visualizations for metrics by phenomena and quantifiers (#32)
    * Calculate metrics by phenomena over all words
    
    * Add heatmaps for phenomena
    
    * Restructure
    
    * Add more heatmaps
    
    * Fix seaborn import
    
    * Replot

---
+++ visualisations/semplotlib.py
@@ -0,0 +1,40 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_confusion_heatmap(data, relative=True):
+    data = pd.DataFrame(data, ["Entailment", "Neutral", "Contradiction"], ["Entailment", "Neutral", "Contradiction"])
+    sns.set(font_scale=1.4)
+    if relative:
+        ax = sns.heatmap(data/data.to_numpy().sum(), cmap="Blues", annot=True, annot_kws={"size": 16}, fmt='.2%')
+    else:
+        ax = sns.heatmap(data, cmap="Blues", annot=True, annot_kws={"size": 16}, fmt=".0f")
+    ax.set(xlabel='Predicted Label', ylabel='True Label')
+    ax.xaxis.set_label_position('top')
+    plt.tick_params(axis='both', which='major', labelbottom=False, bottom=False, top=False, labeltop=True)
+    plt.show()
+
+def plot_metric_heatmap_phenomena(data):
+    phenomena = ["synonym", "antonym", "hypernym", "hyponym", "co_hyponym", "quantifiers", "numericals"]
+    models = ["default", "filtered", "hypothesis-only"]
+    data = pd.DataFrame(data, phenomena, models)
+    
+    sns.set(font_scale=1.4)
+    ax = sns.heatmap(data, cmap="Blues", annot=True, annot_kws={"size": 16}, fmt=".4f")
+    ax.set(xlabel='Model', ylabel='Phenomenon')
+    ax.xaxis.set_label_position('top')
+    plt.tick_params(axis='both', which='major', labelbottom=False, bottom=False, top=False, labeltop=True)
+    plt.show()
+
+
+def plot_metric_heatmap_quantifiers(data):
+    quantifiers = ["all", "any", "each", "few", "many", "much", "no", "several", "some", "whole"]
+    models = ["default", "filtered", "hypothesis-only"]
+    data = pd.DataFrame(data, quantifiers, models)
+    
+    sns.set(font_scale=1.4)
+    ax = sns.heatmap(data, cmap="Blues", annot=True, annot_kws={"size": 16}, fmt=".4f")
+    ax.set(xlabel='Model', ylabel='Quantifier')
+    ax.xaxis.set_label_position('top')
+    plt.tick_params(axis='both', which='major', labelbottom=False, bottom=False, top=False, labeltop=True)
+    plt.show()
