5ccfab397c03a6ba5b0a533897ebeaa8ccad7e51
data-niklas <niklas@4loeser.net>
2023-03-25 10:23:36 +0100

    analyze bias with evaluations (#33)
    * adds ferret bias analysis
    
    * also analyzes ferret evaluations on baseline model
    
    * fix model path
    
    * updates evaluation quantifiers script
    
    * updates evaluation results for hypothesis only
    
    * updates notebook
    
    * rerun with new values
    
    ---------
    
    Co-authored-by: loeser <loeser@login.cl.uni-heidelberg.de>

--- datasets/evaluation_suite_quantifiers_hypothesis_only.py
+++ datasets/evaluation_suite_quantifiers_hypothesis_only.py
@@ -154,8 +154,7 @@ class Suite(evaluate.EvaluationSuite):
             data=data,
             args_for_task={
                 "metric": metric,
-                "input_column": "premise",
-                "second_input_column": "hypothesis",
+                "input_column": "hypothesis",
                 "label_column": "label",
                 "label_mapping": {
                     "ENTAILMENT": 0,
