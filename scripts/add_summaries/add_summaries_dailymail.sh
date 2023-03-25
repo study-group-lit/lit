4281edba8934c9f2ea460dabdccb89b4db5a3bb9
Andre Trump <anditru@web.de>
2023-03-25 02:11:47 +0100

    Add script for preprocessing and adding of summaries

---
+++ scripts/add_summaries/add_summaries_dailymail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=add_summaries_dailymail
+#SBATCH --output=add_summaries_dailymail.txt
+#SBATCH --ntasks=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=32000
+#SBATCH --partition=students
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=batch
+#SBATCH --gres=gpu
+
+
+PATH=/opt/slurm/bin:$PATH
+
+
+source ~/lit/venv/bin/activate
+python3 add_summaries.py -d /workspace/students/lit/datasets/dailymail_dataset
