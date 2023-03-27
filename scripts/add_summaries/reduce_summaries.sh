4d49e297f52b4de5df4a095151b8aeeb700c3ef3
Andre Trump <anditru@web.de>
2023-03-27 13:55:07 +0200

    Parametrize reduction script

--- scripts/add_summaries/reduce_summaries.sh
+++ scripts/add_summaries/reduce_summaries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=reduce_summaries
-#SBATCH --output=reduce_summaries.txt
+#SBATCH --job-name=reduce_cnn_summaries
+#SBATCH --output=reduce_cnn_summaries.txt
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=32000
@@ -12,4 +12,4 @@ PATH=/opt/slurm/bin:$PATH
 
 
 source ~/lit/venv/bin/activate
-python3 reduce_summaries.py
+python3 reduce_summaries.py -i /workspace/students/lit/datasets/cnn_dataset_fixed_summaries -o /workspace/students/lit/datasets/cnn_dataset_fixed_summaries_reduced
