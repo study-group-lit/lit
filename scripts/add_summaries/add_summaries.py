f377db632a544e4e25c8757fa87cfdb0d5426d5c
Andre Trump <anditru@web.de>
2023-03-27 11:58:36 +0200

    Add parameter for output path

--- scripts/add_summaries/add_summaries.py
+++ scripts/add_summaries/add_summaries.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
         prog="Add summaries"
     )
     parser.add_argument('-d', '--dataset_path', required=True)
+    parser.add_argument('-o', '--output_path', required=True)
     parser.add_argument('-p', '--split', required=True)
     parser.add_argument('-s', '--start', required=True)
     parser.add_argument('-e', '--end', required=True)
@@ -26,9 +27,12 @@ if __name__ == "__main__":
     dataset_path = args.dataset_path
     if not os.path.exists(dataset_path):
         raise IOError("Dataset path does not exist")
+    output_path = args.output_path
+    if not os.path.exists(output_path):
+        raise IOError("Output path does not exist")
 
     datasetdict = DatasetDict.load_from_disk(dataset_path)
     summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-6-6")
 
     with_summaries = datasetdict[split].select(range(start_index, end_index+1)).map(add_summary_column, fn_kwargs={"model": summarizer}, batched=True, batch_size=32)
-    with_summaries.save_to_disk(dataset_path=f"/workspace/students/lit/datasets/cnn_dataset_fixed_summaries/{split}_chunk_{start_index}_{end_index}")
+    with_summaries.save_to_disk(dataset_path=f"{output_path}/{split}_chunk_{start_index}_{end_index}")
