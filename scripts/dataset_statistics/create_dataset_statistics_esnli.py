3aa3bcf3bf331410b33de39d55fb44ce2f08b739
data-niklas <niklas@4loeser.net>
2023-03-29 22:19:21 +0200

    add generated dataset statistics to report (#41)
    * add internal dataset statistics plan
    
    * add dataset statistics
    
    * adds tables from generated results
    
    * include short description of datasets
    
    * simplifies row counting
    
    * fixes comments
    
    * changes table hline
    
    * add recast dataset statistics
    
    * add mnli to recast
    
    * adds recast statistics and renames things
    
    ---------
    
    Co-authored-by: loeser <loeser@login.cl.uni-heidelberg.de>

---
+++ scripts/dataset_statistics/create_dataset_statistics_esnli.py
@@ -0,0 +1,47 @@
+import os
+from datasets import load_from_disk
+from evaluate import EvaluationSuite
+from argparse import ArgumentParser
+import json
+from multiprocessing import cpu_count
+import datasets
+
+# Use this if the script crashes due to permissions problems with the cache
+datasets.disable_caching()
+
+DATASET_PREFIX = "/workspace/students/lit/datasets/"
+DATASETS = [
+    DATASET_PREFIX + name for name in [
+        "esnli_phenomena_with_predictions_filtered" # ESNLI samples, which were predicted by the model
+    ]
+]
+
+LABELS = ["entailment", "neutral", "contradiction"]
+
+
+def count_label_occurence(dataset, label):
+    return dataset.filter(lambda record: record["label"] == label, num_proc=cpu_count()).num_rows
+
+def calculate_dataset_counts(dataset_path):
+    if not os.path.exists(dataset_path):
+        raise IOError("Dataset path does not exist")
+
+    dataset = load_from_disk(dataset_path)
+    splits = {"validation": dataset.num_rows}
+    classes = {
+        name: count_label_occurence(dataset, index) for index, name in enumerate(LABELS)
+    }
+    print("Statistics for dataset at " + dataset_path)
+    print(json.dumps(splits))
+    print(json.dumps(classes))
+    print("")
+    with open("./results/" + dataset_path.split("/")[-1] + ".json", "w") as f:
+        f.write(json.dumps(
+            {
+                "splits": splits,
+                "classes": classes
+            }
+        ))
+
+for dataset_path in DATASETS:
+    calculate_dataset_counts(dataset_path)
