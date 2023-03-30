c17d8fbf2f80138c0ed27d02010bdb14a026da57
loeser <loeser@login.cl.uni-heidelberg.de>
2023-03-30 09:06:19 +0200

    remove test script

--- scripts/finetune_recast/test.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-from evaluate import EvaluationSuite
-
-model = pipeline("text-classification", model="/workspace/students/lit/models/roberta-base-finetuned-recast/")
-
-suite = EvaluationSuite.load("../../datasets/evaluation_suite.py")
-print(suite.run(model))
