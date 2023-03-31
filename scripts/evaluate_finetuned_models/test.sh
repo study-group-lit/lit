c65d8a98f4a9e6051dce881a9d28f72f510c4255
anditru <57002492+anditru@users.noreply.github.com>
2023-03-31 09:47:56 +0200

    add results for h5 (#66)
    * adds important results
    
    * Add results for H5
    
    * Apply suggestions
    
    Co-authored-by: imkgerC <imrund08@gmail.com>
    
    * Add interpreation of bad performance on SICK
    
    ---------
    
    Co-authored-by: loeser <loeser@login.cl.uni-heidelberg.de>
    Co-authored-by: imkgerC <imrund08@gmail.com>

---
+++ scripts/evaluate_finetuned_models/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=test_sick_test
+#SBATCH --output=test.txt
+#SBATCH --ntasks=1
+#SBATCH --time=6:00:00
+#SBATCH --mem=32000
+#SBATCH --partition=students
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=batch
+#SBATCH --gres=gpu
+
+
+PATH=/opt/slurm/bin:$PATH
+
+
+source ../../venv/bin/activate
+python3 test.py
